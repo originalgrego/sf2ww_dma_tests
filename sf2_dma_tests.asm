2c402159ff776cdfaaf5c17dbefcd3bf7a397b16
Grego Lewandowski <original_grego@yahoo.com>
2021-01-26 23:42:41 -0500

    Beginning of object dma test.

--- sf2_dma_tests.asm
+++ sf2_dma_tests.asm
@@ -1,5 +1,53 @@
  org  0
-   incbin  "build\sf2.bin"
+  incbin  "build\sf2.bin"
    
  org $8D1
-   dc.b "TEST  "
+  dc.b "123  "
+   
+ org $000A54
+  jmp main
+
+ ; Don't fade out test screen 
+ org $000984
+;   NOP
+;   NOP
+ 
+ ; Free space
+ org $0E0000
+main:
+  moveq #$0, D0
+  move.w #$4f0, D0
+  movea.l #$00910000, A0
+  movea.l #sf2_objects_2, A1
+  
+  bsr upload_gfx
+
+  move.w #$4f0, D0
+  movea.l #$00918000, A0
+  movea.l #sf2_objects, A1
+  
+  bsr upload_gfx
+  
+loop:
+  
+  move.w  #$9100, ($2a,A5)
+  move.w  #$9180, ($2a,A5)
+
+  bra loop
+  
+  
+;-----------------
+upload_gfx:
+  move.b  (A1, D0.w), D1
+  move.b  D1, (A0, D0.w)
+  dbra D0, upload_gfx
+  rts
+;-----------------
+  
+  
+sf2_objects:
+  incbin "sf2_objects_1.bin"
+ 
+sf2_objects_2:
+  incbin "sf2_objects_2.bin"
+ 
